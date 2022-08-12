86105a508538915c8b765a5e391485998e9e22f8
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-11 20:22:39 -0400

    data 1

--- bootcam_my_first_app.py
+++ bootcam_my_first_app.py
@@ -7,9 +7,25 @@ import numpy as np
 
 st.title('Class 2 > Mojix bootcamp')
 
-st.write("Tutorial")
+st.subheader('Tutorial')
 
-st.write("Splitting a string")
+
+st.write("1. Walrus operator")
+
+"""
+The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+"""
+
+
+
+with st.echo(code_location='below'):
+   Mylist = [1,2,3]
+   if(l := len(mylist) > 2)
+   print(l)
+
+
+
+st.write("2. Splitting a string")
 
 """
 If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!
