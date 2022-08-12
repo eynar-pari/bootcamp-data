f1d5fba910b548faa14930881c707ea3557465ab
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-11 20:32:09 -0400

    data 1

--- bootcam_my_first_app.py
+++ bootcam_my_first_app.py
@@ -10,7 +10,7 @@ st.title('Class 2 > Mojix bootcamp')
 st.subheader('Tutorial')
 
 
-st.write("1. Walrus operator")
+st.caption("1. Walrus operator")
 
 """
 The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
@@ -23,7 +23,7 @@ st.code(code, language='python')
 
   
 
-st.write("2. Splitting a string")
+st.caption("2. Splitting a string")
 
 """
 If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!
@@ -34,7 +34,7 @@ code = ''' string = “hello world”
 string.split()'''
 st.code(code, language='python')
 
-st.write("3. Reversing a string")
+st.caption("3. Reversing a string")
 
 """
 If you want to reverse a given string, you can do that with only one line of code using the negative indexing of the string.
