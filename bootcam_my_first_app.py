cd2685d6d5b828d17a081a6c869b126f5f490dd4
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-11 20:12:58 -0400

    data 1

--- bootcam_my_first_app.py
+++ bootcam_my_first_app.py
@@ -1,3 +1,19 @@
 import streamlit as st 
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
 
 st.write("HELLO FROM THE MOJIX BOOTCAMP")
+
+st.write("Splitting a string")
+
+"""
+If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!
+"""
+
+
+with st.echo(code_location='below'):
+   string = “hello world”
+   string.split()
