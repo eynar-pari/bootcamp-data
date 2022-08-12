db9abe52dc25007066f5576a105e59358e36f93c
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-08-11 20:44:56 -0400

    data 1

--- bootcam_my_first_app.py
+++ bootcam_my_first_app.py
@@ -65,3 +65,7 @@ df = pd.DataFrame(
 
 st.table(df)
 
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
